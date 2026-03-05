91f60c9344ecfeba195d6c3bec99efe4aa23511f
Matheus-Alcantara-dev <matheusalcantarad3v@gmail.com>
2026-03-05 17:35:05 -0300

    Commit inicial: configuração do Spring Boot

---
+++ src/test/java/com/theus/InventarioTI/InventarioTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theus.InventarioTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
